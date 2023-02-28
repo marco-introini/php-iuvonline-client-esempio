63ba683bc9fd40fa6d873957a55ba94691428b91
Marco Introini <89445565+marco-introini@users.noreply.github.com>
2023-02-28 10:04:16 +0100

    First release

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Esempio di client PHP per accedere a IUVOnline
+# Esempio di client PHP per accedere a IUVOnline BPS
 
+### Installazione wsdl
+
+Inserire il pacchetto wsd e xsd inviato dalla banca nella directory wsdl. Il risultato è il seguente:
+
+![wsdl.png](img%2Fwsdl.png)
+
+Modificare il wsdl immettendo l'endpoint inviato
+
+### Installazione certificati
+
+Copiare i certificati inviati nella directory cert. Il risultato sarà il seguente:
+
+![certificati.png](img%2Fcertificati.png)
+
+### Environment
+
+Rinominare ed eventualmente modificare il file .env.example in .env
+
+### Composer
+
+Eseguire `composer install` per installare le dipendenze
+
+### Chiamata Effettiva
+
+Ora è possibile eseguire il file chiamata.php dentro la directory src
--- src/chiamata.php
+++ src/chiamata.php
@@ -1,1 +1,40 @@
 <?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..');
+$dotenv->load();
+
+echo "Chiamata a ".$_ENV['URL'];
+
+$options = [
+    'soap_version' => SOAP_1_1,
+    'trace' => true,
+    'keep_alive' => false,
+    'connection_timeout' => 10000,
+    'cache_wsdl' => WSDL_CACHE_NONE,
+    'exceptions' => 0,
+    'authentication' => SOAP_AUTHENTICATION_DIGEST,
+    'stream_context' => stream_context_create([
+        'ssl' => [
+            'crypto_method' => STREAM_CRYPTO_METHOD_TLSv1_2_CLIENT,
+            'cafile' => $_ENV['CA_CERT'],
+            'local_cert' => $_ENV['AUTH_CERT'],
+            'local_pk' => $_ENV['AUTH_KEY'],
+            'verify_peer' => true,
+            'verify_peername' => false,
+            'allow_self_signed' => true
+        ]
+    ])
+];
+
+$soapClient = new SoapClient('../wsdl/wsdl/IUVOnlineService-scrittura-wsdl-1.3.wsdl', $options);
+$xml = file_get_contents('../xml/create.xml');
+$args = array();
+if (!empty($xml)) {
+    $args = array(new \SoapVar($xml, XSD_ANYXML));
+}
+
+$response = $soapClient->__soapCall('IUVOnlineCreate', $args);
+
+echo $response;
